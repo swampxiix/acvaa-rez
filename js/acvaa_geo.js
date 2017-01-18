3abe29f66b50f9d8b31759280e7bea4ecd5ffed1
Gary Perez <gary@eringary.com>
2017-01-17 21:11:04 -0500

    None of this works.

--- js/acvaa_geo.js
+++ js/acvaa_geo.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 //------------------------------------------------------------------------------
 
 function insert_input () {
-    var txthtml = '<input type="text" name="state" value="" class="input">';
+    var txthtml = '<input type="text" name="state" value="" class="input" id="state">';
     $("#state_slot").empty();
     $("#state_slot").html(txthtml);
 };
@@ -13,7 +13,7 @@ function populate_pulldown (pullhtml) {
 };
 
 function construct_pulldown (state_order, state_dict) {
-    var pullhtml = '<select name="state" class="input">';
+    var pullhtml = '<select name="state" class="input" id="state">';
     var i;
     for (i = 0; i < state_order.length; i++) {
         var state_name = state_order[i];
@@ -51,14 +51,41 @@ $("select#countryselect").change(function () {
     get_states( $("select#countryselect option:selected").text() );
 });
 
-// ON PAGE LOAD
-get_states("USA");
+function alter_pulldowns_for_me (mycountry, mystate) {
+    console.log(mycountry);
+    console.log(mystate);
+    get_states(mycountry);
+    $("select#countryselect").val(mycountry);
+    $("select#state > option").each(function() {
+        console.log(this.text + ' ' + this.value);
+    });
 
-
-function select_country (mycountry) {
-    console.log('mycountry is' + mycountry);
 
 };
+
+function get_my_country_and_state () {
+    var username = $.cookie('username');
+    $.ajax( {
+        url: 'ajax_get_location',
+        data: 'username=' + username,
+        timeout: 3000,
+        dataType:'json',
+        error: function(x, s, e) {
+        alert('error in get country and state ajax call');
+        },
+        success: function(d, s, x) {
+            var mycountry = d.country;
+            var mystate = d.state;
+            alter_pulldowns_for_me(mycountry, mystate);
+        }
+    });
+};
+
+
+// ON PAGE LOAD
+//get_states("USA");
+get_my_country_and_state();
+
 
 //------------------------------------------------------------------------------
 });
